b25881869177619598f73e617a928ae45351b8e5
Tom Kubasik <tom.kubasik@gmail.com>
2021-06-29 23:07:17 +0200

    animation on pop but fade out ...

--- server/index.js
+++ server/index.js
@@ -8,9 +8,12 @@ const io = require("socket.io")(server, {
 	},
 });
 
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", (req, res) => {
 	res.sendFile(__dirname + "/index.html");
 });
+
 
 io.on("connection", (socket) => {
 	console.log("a user connected");
