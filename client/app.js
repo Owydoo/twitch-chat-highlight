9c140b404678651d795d49818d7ae9100b6ceac8
Tom Kubasik <tom.kubasik@gmail.com>
2021-06-29 22:36:57 +0200

    display and hide in Obs

--- client/app.js
+++ client/app.js
@@ -1,25 +1,28 @@
-const userElement = document.querySelector('#user')
-const messageElement = document.querySelector('#message')
-
+const userElement = document.querySelector("#user");
+const messageElement = document.querySelector("#message");
 
 const client = new tmi.Client({
-	channels: [ 'sardoche' ]
+	channels: ["sardoche"],
 });
 
 client.connect();
 
-client.on('message', (channel, tags, message, self) => {
-	// "Alca: Hello, World!"
-	console.log(`${tags['display-name']}: ${message}`);
-    userElement.textContent = tags['display-name']
-    messageElement.textContent = message
+client.on("message", (channel, tags, message, self) => {
+	console.log(`${tags["display-name"]}: ${message}`);
+	userElement.textContent = tags["display-name"];
+	messageElement.textContent = message;
 });
 
+var buttonSend = document.getElementById("buttoncard");
+buttonSend.onclick = () => {
+	var username = document.getElementById("user").innerText;
+	var message = document.getElementById("message").innerText;
+	socket.emit("chat", username, message);
+};
 
-var button = document.getElementById("buttoncard");
-  button.onclick = () => {
-	var username = document.getElementById("user").innerText
-	var message = document.getElementById("message").innerText
-    socket.emit("chat",username, message)
-  }
-				
+
+var buttonHide = document.getElementById("buttonhide");
+buttonHide.onclick = () => {
+	socket.emit("hideChat");
+};
+
--- server/index.js
+++ server/index.js
@@ -1,26 +1,31 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-      origin: '*',
-    }
-  });
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+	},
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.emit("message", "oueeeeee")
-  
-  socket.on("chat", (username, message) => {
-    console.log(`${username} a envoyé ${message}`)
-  })
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+	console.log("a user connected");
+	socket.emit("message", "oueeeeee");
+
+	socket.on("chat", (username, message) => {
+		console.log(`${username} a envoyé ${message}`);
+		io.emit("displayChat", username, message);
+	});
+
+	socket.on("hideChat", () => {
+		io.emit("hideChat")
+	})
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+	console.log("listening on *:3000");
 });
